a0b5d800052ce8b37ae4d8fe93ba0cf302feea6c
Dyaoss <eogus12003@naver.com>
2024-06-28 12:44:37 +0900

    Lv1 구현

--- app/src/main/java/com/example/loginandintroduce/SignInActivity.kt
+++ app/src/main/java/com/example/loginandintroduce/SignInActivity.kt
@@ -1,6 +1,10 @@
 package com.example.loginandintroduce
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,31 @@ class SignInActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val logIn = findViewById<Button>(R.id.btn_login)
+        val sign = findViewById<Button>(R.id.btn_signup)
+
+        val extraId = findViewById<EditText>(R.id.et_id) // id editText의 입력값을 extra에 저장
+        val extraPW = findViewById<EditText>(R.id.et_password)
+
+        logIn.setOnClickListener {
+
+            var getId = extraId.text.toString()
+            var getPassWord = extraPW.text.toString()
+            if (getId.isEmpty()) {
+                Toast.makeText(applicationContext, "아이디를 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else if (getPassWord.isEmpty()) {
+                Toast.makeText(applicationContext, "패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent) // 로그인 버튼을 누르면 HomeActivity로 넘어감
+                Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+            sign.setOnClickListener {
+                val intent = Intent(this, SignUpActivity::class.java)
+                startActivity(intent)
+            } // 회원가입 버튼을 누르면 SignInActivity로 넘어감
+        }
     }
-}
