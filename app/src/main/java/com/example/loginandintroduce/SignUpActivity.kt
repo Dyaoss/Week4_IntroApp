7e783d36871c9badce7a195f00732d41d4c145b0
Dyaoss <eogus12003@naver.com>
2024-07-01 11:03:00 +0900

    회원가입페이지 수정 - 비밀번호 확인 추가

--- app/src/main/java/com/example/loginandintroduce/SignUpActivity.kt
+++ app/src/main/java/com/example/loginandintroduce/SignUpActivity.kt
@@ -22,22 +22,34 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         val signUp = findViewById<Button>(R.id.btn_signup2)
+        val closeSignUp = findViewById<Button>(R.id.btn_close2)
 
         val nameInfo = findViewById<EditText>(R.id.et_sign_up_name)
         val idInfo = findViewById<EditText>(R.id.et_sign_up_ID)
         val passWordInfo = findViewById<EditText>(R.id.et_sign_up_password)
+        val passsWordAgainInfo = findViewById<EditText>(R.id.et_sign_up_password_again)
 
         signUp.setOnClickListener {
             var getSignUpName = nameInfo.text.toString()
             var getSignUpId = idInfo.text.toString()
             var getSignUpPW = passWordInfo.text.toString()
+            var getSignUpPWAgain = passsWordAgainInfo.text.toString()
 
-            if(getSignUpName.isEmpty() || getSignUpId.isEmpty() || getSignUpPW.isEmpty()){
+            if(getSignUpName.isEmpty() || getSignUpId.isEmpty() || getSignUpPW.isEmpty() || getSignUpPWAgain.isEmpty()){
                 Toast.makeText(applicationContext, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(applicationContext, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
-                finish()
+                if (getSignUpPW != getSignUpPWAgain){
+                    Toast.makeText(applicationContext, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                }else {
+                    Toast.makeText(applicationContext, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
             }
         }
+
+        closeSignUp.setOnClickListener {
+            finish()
+        }
+
     }
 }
